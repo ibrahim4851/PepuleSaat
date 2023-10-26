f20a79b979fc82671958f1dffb6c8cc55fd09f8e
ibrahim4851 <erogluih2001@gmail.com>
2023-10-26 16:54:30 +0300

    Credit Card Update

--- app/src/main/java/com/sultan/pepulesaat/presentation/ui/payment/CreditCard.kt
+++ app/src/main/java/com/sultan/pepulesaat/presentation/ui/payment/CreditCard.kt
@@ -20,51 +20,6 @@ import com.steliospapamichail.creditcardmasker.utils.getCardTypeFromNumber
 import com.steliospapamichail.creditcardmasker.viewtransformations.CardNumberMask
 
 @Composable
-fun CreditCardTextField(
-    value: String,
-    onValueChange: (String) -> Unit
-) {
-    var cursorPosition by remember { mutableStateOf(0) }
-
-    var formattedValue by remember(value, cursorPosition) {
-        mutableStateOf(formatCreditCard(value, cursorPosition))
-    }
-
-    OutlinedTextField(
-        value = formattedValue,
-        label = { Text(text = "Kredi Kartı Numarası")},
-        onValueChange = {
-            val filteredValue = it.filter { it.isDigit() }
-            cursorPosition = it.length
-            formattedValue = formatCreditCard(filteredValue, cursorPosition)
-            onValueChange(filteredValue)
-        },
-        keyboardOptions = KeyboardOptions.Default.copy(
-            keyboardType = KeyboardType.Number
-        ),
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(16.dp)
-    )
-}
-
-private fun formatCreditCard(input: String, cursorPosition: Int): String {
-    val digitsOnly = input.filter { it.isDigit() }
-    val formatted = buildString {
-        digitsOnly.chunked(4).forEachIndexed { index, chunk ->
-            if (index > 0) append(" ")
-            append(chunk)
-        }
-    }
-    val adjustedCursorPosition = cursorPosition + cursorPosition / 4
-    return if (adjustedCursorPosition < formatted.length) {
-        formatted.substring(0, adjustedCursorPosition) + " " + formatted.substring(adjustedCursorPosition)
-    } else {
-        formatted
-    }
-}
-
-@Composable
 fun CardNumber() {
     var number by remember { mutableStateOf("") }
     OutlinedTextField(
@@ -91,16 +46,5 @@ fun CardNumber() {
                     .padding(end = 10.dp)
             )
         }
-    )
-}
-
-
-@Preview(showBackground = true)
-@Composable
-fun CreditCardTextFieldPreview() {
-    var creditCardNumber by remember { mutableStateOf("") }
-    CreditCardTextField(
-        value = creditCardNumber,
-        onValueChange = { creditCardNumber = it }
     )
 }
